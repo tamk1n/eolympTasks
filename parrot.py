664c7810901aba80f3bef06a0b71e930e6bebfb5
tamk1n <109694582+tamk1n@users.noreply.github.com>
2022-09-28 13:15:34 +0000

    Deleting files

--- parrot.py
+++
@@ -1,8 +0,0 @@
-message = ""
-
-while message.lower() != "end":
-  message = input("Enter your sentence:")
-  print(f"\n{message}")
-  print("\n")
-  
-print("\nThank you!")
--- user_registration (copy).py
+++
@@ -1,9 +0,0 @@
-new_users = ["Tamkin", "Ramil", "Gasim", "Eyvaz"]
-users=[]
-
-while len(new_users) !=0:
-  user = new_users.pop()
-  users.append(user)
-
-users.sort(reverse=True)
-print(f" Registered users: {users}")
