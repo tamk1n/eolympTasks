04a0d8dd46f8bfe1b31c0817faf92746f42ebb77
Tamkin Tamrazli <tamkintamraz@gmail.com>
2022-10-03 23:42:38 +0400

    Update dice with flexible sides

--- dice.py
+++ dice.py
@@ -2,15 +2,14 @@ from random import randint
 
 class Dice:
 
-    def _init__(self, sides):
+    def __init__(self, sides):
         self.sides = sides
-        sides = 6
 
     def roll_dice(self):
-        for x in range(0,6):
-            side = randint(1,6)
+        for x in range(0,10):
+            side = randint(1,self.sides)
             print(side)
 
-my_game = Dice()
+my_game = Dice(5)
 
 my_game.roll_dice()
