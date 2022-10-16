4d479372034880981fb0dd912244dfd1e0e3ed2d
Tamkin Tamrazli <tamkintamraz@gmail.com>
2022-10-16 11:38:42 +0400

    Add apps for codewar tasks

---
+++ example.py
@@ -0,0 +1,3 @@
+name = "Tamkin"
+
+print(name[::-1])
--- reverse_string.py
+++ reverse_string.py
@@ -1,18 +1,11 @@
 def spin_words(sentence):
-#sentence = "My name is Tamkin"
     sentList=sentence.split()
     newSent =[]
 
     for word in sentList:
         if len(word) >4:
-            wordList = []
-            for letter in word:
-                wordList.append(letter)
-            wordList.reverse()
-            rWord =""
-            for rletter in wordList:
-                rWord+=rletter
-            newSent.append(rWord)
+            revWord = word[::-1]
+            newSent.append(revWord)
         else:
             newSent.append(word)
 
